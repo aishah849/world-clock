5fc2bd6b25f69a7a28e420156fb36859eab92827
aishah849 <aishahalrashid@gmail.com>
2023-01-06 14:07:17 +0800

    Added the functionality for the select

--- javascript/index.js
+++ javascript/index.js
@@ -1,28 +1,51 @@
 function updateTime() {
   //Singapore
   let singaporeElement = document.querySelector("#singapore");
-  let singaporeDateElement = singaporeElement.querySelector(".date");
-  let singaporeTimeElement = singaporeElement.querySelector(".time");
-  let singaporeTime = moment().tz("Asia/Singapore");
+  if (singaporeElement) {
+    let singaporeDateElement = singaporeElement.querySelector(".date");
+    let singaporeTimeElement = singaporeElement.querySelector(".time");
+    let singaporeTime = moment().tz("Asia/Singapore");
 
-  singaporeDateElement.innerHTML = singaporeTime.format("D MMMM YYYY");
-  //singaporeTimeElement.innerHTML = `${singaporeTime.format("h:mm:ss")} <small>${singaporeTime.format("A")}</small>`;
-  singaporeTimeElement.innerHTML = singaporeTime.format(
-    "h:mm:ss [<small>]A[<small/]"
-  );
+    singaporeDateElement.innerHTML = singaporeTime.format("D MMMM YYYY");
+    //singaporeTimeElement.innerHTML = `${singaporeTime.format("h:mm:ss")} <small>${singaporeTime.format("A")}</small>`;
+    singaporeTimeElement.innerHTML = singaporeTime.format(
+      "h:mm:ss [<small>]A[<small/]"
+    );
+  }
 
   //Colombo
   let colomboElement = document.querySelector("#colombo");
-  let colomboDateElement = colomboElement.querySelector(".date");
-  let colomboTimeElement = colomboElement.querySelector(".time");
-  let colomboTime = moment().tz("Asia/Colombo");
+  if (colomboElement) {
+    let colomboDateElement = colomboElement.querySelector(".date");
+    let colomboTimeElement = colomboElement.querySelector(".time");
+    let colomboTime = moment().tz("Asia/Colombo");
 
-  colomboDateElement.innerHTML = colomboTime.format("D MMMM YYYY");
-  //colomboTimeElement.innerHTML = `${colomboTime.format("h:mm:ss")} <small>${colomboTime.format("A")}</small>`;
-  colomboTimeElement.innerHTML = colomboTime.format(
-    "h:mm:ss [<small>]A[<small/]"
-  );
+    colomboDateElement.innerHTML = colomboTime.format("D MMMM YYYY");
+    //colomboTimeElement.innerHTML = `${colomboTime.format("h:mm:ss")} <small>${colomboTime.format("A")}</small>`;
+    colomboTimeElement.innerHTML = colomboTime.format(
+      "h:mm:ss [<small>]A[<small/]"
+    );
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("D MMMM YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )}<small> ${cityTime.format("A")}<small/></div></div>`;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
